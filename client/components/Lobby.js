9ce0791453fdcbce77000830b50607733dc86ba7
Joe Bandenburg <joe.bandenburg@forgerock.com>
2015-08-07 08:30:27 +0100

    Give the lobby a little bit of style

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,12 +1,33 @@
 import React from "react";
+import mui from "material-ui";
 
 export default class Lobby extends React.Component {
     render() {
+        const containerStyle = {
+            position: "absolute",
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: this.context.muiTheme.component.appBar.color
+        };
+        const codeStyle = {
+            fontSize: 120,
+            lineHeight: "160px",
+            fontWeight: 100
+        };
         return (
-            <div>
-                <div>{this.props.gameCode}</div>
-                <button onClick={this.props.onStart}>Start!</button>
+            <div style={containerStyle}>
+                <div style={codeStyle}>{this.props.gameCode}</div>
+                <mui.RaisedButton label="Start" onClick={this.props.onStart}></mui.RaisedButton>
             </div>
         );
     }
 }
+Lobby.contextTypes = {
+    muiTheme: React.PropTypes.object
+};
